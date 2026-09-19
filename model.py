32870a82d239819fdcc4157dd1a0c6d9660492ea
Srikesh <81523080+srikesh3005@users.noreply.github.com>
2026-09-19 22:35:47 +0530

    Step 7: decode_ids

--- model.py
+++ model.py
@@ -44,8 +44,11 @@ def decode_int(token_id, itos):
     return itos[token_id]
     pass
 
-# Step 7 - decode_ids (not yet solved)
-# TODO: implement
+# Step 7 - decode_ids
+def decode_ids(ids, itos):
+    """Decode a list of token ids into a string using itos."""
+    return ''.join(decode_int(token_id, itos) for token_id in ids)
+    pass
 
 # Step 8 - make_1d_array (not yet solved)
 # TODO: implement
