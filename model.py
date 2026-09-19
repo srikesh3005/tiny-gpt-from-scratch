31eb550c1308639efa6136742dae6cc69e24ed4f
Srikesh <81523080+srikesh3005@users.noreply.github.com>
2026-09-19 20:51:36 +0530

    Step 4: encode_char

--- model.py
+++ model.py
@@ -25,8 +25,12 @@ def build_itos(vocab):
     return {i:ch for i ,ch in enumerate(vocab)}# TODO: build an int-to-string lookup from the vocab list
     pass
 
-# Step 4 - encode_char (not yet solved)
-# TODO: implement
+# Step 4 - encode_char
+def encode_char(ch, stoi):
+    """Return the integer token id for a single character ch using stoi."""
+    # TODO: look up ch in the stoi mapping and return its id
+    return stoi[ch]
+    pass
 
 # Step 5 - encode_string (not yet solved)
 # TODO: implement
